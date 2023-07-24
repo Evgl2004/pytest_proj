18228cfd5cdca4971807079f16b9c91a1b639d71
Andrey Evgl <evgl2004@yandex.ru>
2023-07-24 20:37:59 +0500

    Внесены изменения в тесты "test_slice"

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,3 +9,6 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([43, 211, 1, 0], -2, 3) == [1]
+    assert arrs.my_slice([9, 10, 8, 11], -6, -2) == [9, 10]
+    assert arrs.my_slice([]) == []
